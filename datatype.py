34593dce89a48e950e6a49d124f93464c531d1be
diyaparekh1430 <diyaparekh1430@gmail.com>
2026-03-31 20:28:13 +0530

    Add files via upload

--- datatype.py
+++ datatype.py
@@ -1,11 +1,41 @@
-print("Hello world.!!")
-#integer
-a=21
-print("a=",a)
-print("datatype of a:",type(a))
+a="Hello World!"
+print(a)
 
-#float
-b=30.5
-print("\nb=",b)
-print("datatype of b:",type(b))
+b=20
+print(b)
 
+c=12.5
+print(c)
+
+d=["apple","banana","cherry"]
+print(d)
+
+e=("apple","banana","cherry")
+print(e)
+
+f=range(5)
+print(f)
+
+g={"Name":"johan","age":36}
+print(g)
+
+h={"apple","banana","cheryy"}
+print(h)
+
+i=({"apple","banana","cherry"})
+print(i)
+
+j=True
+print(j)
+
+k=b"Hello"
+print(k)
+
+l=bytearray(5)
+print(l)
+
+m=memoryview(bytes(5))
+print(m)
+
+n=none
+print(n)
